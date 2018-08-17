dbcb6776572d4f71579711e27750dc1dec08ba2e
Melissa Knudtson <melissa.erin.knudtson@gmail.com>
2018-08-16 22:32:02 -0400

    add local storage

--- app.js
+++ app.js
@@ -4,8 +4,7 @@
     // can be pretty short
 // __ game w/inputs - keep as much of the orig game as poss
 // __ randomisation - rather than for loops
-// __ local storage - for user-inputted vocabulary
-
+// _x_ local storage - for user-inputted vocabulary
 
 // test - html file connected to js file?
 console.log("I'm connected!");
@@ -32,14 +31,20 @@ let vocabulary = []; // array of objects
 function createAndShow(){
     let inputEN = document.getElementById('EN').value;
     let inputFR = document.getElementById('FR').value;
-    vocabulary.push({ // push objects to array
+    vocabulary.push({ // push object to array
         EN: inputEN,
         FR: inputFR
     });
     document.getElementById('EN').value = ''; // clear input field
     document.getElementById('FR').value = ''; // clear input field
-    // console.log(vocabulary); // initial testing
+    console.log(vocabulary); // initial testing
     showVocabList();
+    // add to localStorage by looping
+    for (let i = 0; i < vocabulary.length; i++) {
+        localStorage.setItem('pair', [vocabulary[i].EN, vocabulary[i].FR]); // 
+        var localPair = localStorage.getItem('pair')
+        console.log(localPair);
+    }
 }
 
 // separate function, in case i don't always want to show the list
@@ -67,112 +72,112 @@ var LastQuestionResults = "";
 
  
 
-for (i = 0; i < arrayENtoFR.length; i++) {
+// for (i = 0; i < arrayENtoFR.length; i++) {
 
-    var inputBox = prompt(LastQuestionResults + "What is the French word for " + arrayENtoFR[i][0] + "?");
+//     var inputBox = prompt(LastQuestionResults + "What is the French word for " + arrayENtoFR[i][0] + "?");
 
-    if ((inputBox === null) || (inputBox === "")) {
+//     if ((inputBox === null) || (inputBox === "")) {
 
-        LastQuestionResults = "";
+//         LastQuestionResults = "";
 
-        ExitLoop = true;
+//         ExitLoop = true;
 
-        break;
+//         break;
 
-    }
+//     }
 
-    else if ((inputBox.toLowerCase() === arrayENtoFR[i][1].toLowerCase()) || (inputBox.toLowerCase() === arrayENtoFR[i][2].toLowerCase()) || (inputBox.toLowerCase() === arrayENtoFR[i][3].toLowerCase())) {
+//     else if ((inputBox.toLowerCase() === arrayENtoFR[i][1].toLowerCase()) || (inputBox.toLowerCase() === arrayENtoFR[i][2].toLowerCase()) || (inputBox.toLowerCase() === arrayENtoFR[i][3].toLowerCase())) {
 
-        rightAnswers++;
+//         rightAnswers++;
 
-        LastQuestionResults = "Correct!" + "\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n"
+//         LastQuestionResults = "Correct!" + "\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n"
 
-//        alert("Correct!" + "\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct");
+// //        alert("Correct!" + "\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct");
 
-    }
+//     }
 
-    else {
+//     else {
 
-        wrongAnswers++;
+//         wrongAnswers++;
 
-        if (arrayENtoFR[i][3] !== "")
+//         if (arrayENtoFR[i][3] !== "")
 
-            LastQuestionResults = "Sorry, the correct answer is " + arrayENtoFR[i][1] + " or " + arrayENtoFR[i][2] + " or " + arrayENtoFR[i][3] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
+//             LastQuestionResults = "Sorry, the correct answer is " + arrayENtoFR[i][1] + " or " + arrayENtoFR[i][2] + " or " + arrayENtoFR[i][3] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
 
-//            alert("Sorry, the correct answer is " + arrayENtoFR[i][1] + " or " + arrayENtoFR[i][2] + " or " + arrayENtoFR[i][3] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
+// //            alert("Sorry, the correct answer is " + arrayENtoFR[i][1] + " or " + arrayENtoFR[i][2] + " or " + arrayENtoFR[i][3] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
 
-        else if (arrayENtoFR[i][2] !== "")
+//         else if (arrayENtoFR[i][2] !== "")
 
-            LastQuestionResults = "Sorry, the correct answer is " + arrayENtoFR[i][1] + " or " + arrayENtoFR[i][2] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
+//             LastQuestionResults = "Sorry, the correct answer is " + arrayENtoFR[i][1] + " or " + arrayENtoFR[i][2] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
 
-//            alert("Sorry, the correct answer is " + arrayENtoFR[i][1] + " or " + arrayENtoFR[i][2] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
+// //            alert("Sorry, the correct answer is " + arrayENtoFR[i][1] + " or " + arrayENtoFR[i][2] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
 
-        else
+//         else
 
-            LastQuestionResults = "Sorry, the correct answer is " + arrayENtoFR[i][1] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
+//             LastQuestionResults = "Sorry, the correct answer is " + arrayENtoFR[i][1] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
 
-//            alert("Sorry, the correct answer is " + arrayENtoFR[i][1] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
+// //            alert("Sorry, the correct answer is " + arrayENtoFR[i][1] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
 
-    }
+//     }
 
-}
-
- 
-
-if (ExitLoop == false)
-
-{
-
-    for (i = 0; i < arrayFRtoEN.length; i++) {
-
-        var inputBox = prompt(LastQuestionResults + "What is the English word for " + arrayFRtoEN[i][0] + "?");
-
-        if ((inputBox === null) || (inputBox === "")) {
-
-            LastQuestionResults = "";
-
-            break;
-
-        }
-
-        else if ((inputBox.toLowerCase() === arrayFRtoEN[i][1].toLowerCase()) || (inputBox.toLowerCase() === arrayFRtoEN[i][2].toLowerCase()) || (inputBox.toLowerCase() === arrayFRtoEN[i][3].toLowerCase())) {
-
-            rightAnswers++;
-
-            LastQuestionResults = "Correct!" + "\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
-
-//            alert("Correct!" + "\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
-
-        }
-
-        else {
-
-            wrongAnswers++;
-
-            if (arrayFRtoEN[i][3] !== "")
-
-                LastQuestionResults = "Sorry, the correct answer is " + arrayFRtoEN[i][1] + " or " + arrayFRtoEN[i][2] + " or " + arrayFRtoEN[i][3] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
-
-//              alert("Sorry, the correct answer is " + arrayFRtoEN[i][1] + " or " + arrayFRtoEN[i][2] + " or " + arrayFRtoEN[i][3] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
-
-            else if (arrayENtoFR[i][2] !== "")
-
-                LastQuestionResults = "Sorry, the correct answer is " + arrayFRtoEN[i][1] + " or " + arrayFRtoEN[i][2] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
-
-//              alert("Sorry, the correct answer is " + arrayFRtoEN[i][1] + " or " + arrayFRtoEN[i][2] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
-
-            else
-
-                LastQuestionResults = "Sorry, the correct answer is " + arrayFRtoEN[i][1] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
-
-//              alert("Sorry, the correct answer is " + arrayFRtoEN[i][1] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
-
-        }
-
-    }
-
-}
+// }
 
  
 
-alert(LastQuestionResults + "You finished with " + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
+// if (ExitLoop == false)
+
+// {
+
+//     for (i = 0; i < arrayFRtoEN.length; i++) {
+
+//         var inputBox = prompt(LastQuestionResults + "What is the English word for " + arrayFRtoEN[i][0] + "?");
+
+//         if ((inputBox === null) || (inputBox === "")) {
+
+//             LastQuestionResults = "";
+
+//             break;
+
+//         }
+
+//         else if ((inputBox.toLowerCase() === arrayFRtoEN[i][1].toLowerCase()) || (inputBox.toLowerCase() === arrayFRtoEN[i][2].toLowerCase()) || (inputBox.toLowerCase() === arrayFRtoEN[i][3].toLowerCase())) {
+
+//             rightAnswers++;
+
+//             LastQuestionResults = "Correct!" + "\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
+
+// //            alert("Correct!" + "\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
+
+//         }
+
+//         else {
+
+//             wrongAnswers++;
+
+//             if (arrayFRtoEN[i][3] !== "")
+
+//                 LastQuestionResults = "Sorry, the correct answer is " + arrayFRtoEN[i][1] + " or " + arrayFRtoEN[i][2] + " or " + arrayFRtoEN[i][3] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
+
+// //              alert("Sorry, the correct answer is " + arrayFRtoEN[i][1] + " or " + arrayFRtoEN[i][2] + " or " + arrayFRtoEN[i][3] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
+
+//             else if (arrayENtoFR[i][2] !== "")
+
+//                 LastQuestionResults = "Sorry, the correct answer is " + arrayFRtoEN[i][1] + " or " + arrayFRtoEN[i][2] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
+
+// //              alert("Sorry, the correct answer is " + arrayFRtoEN[i][1] + " or " + arrayFRtoEN[i][2] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
+
+//             else
+
+//                 LastQuestionResults = "Sorry, the correct answer is " + arrayFRtoEN[i][1] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.\n\n";
+
+// //              alert("Sorry, the correct answer is " + arrayFRtoEN[i][1] + ".\n" + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
+
+//         }
+
+//     }
+
+// }
+
+ 
+
+// alert(LastQuestionResults + "You finished with " + rightAnswers + " out of " + (rightAnswers + wrongAnswers) + " correct.");
